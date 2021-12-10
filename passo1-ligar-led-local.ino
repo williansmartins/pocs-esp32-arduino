004a95c3f6d3577289f892dad1f12773b6c48670
Willians Martins <professorwillians@gmail.com>
2021-12-10 11:04:16 -0300

    Create passo1-ligar-led-local.ino

---
+++ passo1-ligar-led-local.ino
@@ -0,0 +1,67 @@
+#include <WiFi.h>
+ 
+const char* ssid = "JRFiber-NAYARA";
+const char* password = "xxxxx";
+int LED = 2;
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+ 
+  Serial.println();
+  Serial.print("Conectando-se a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    Serial.print(WiFi.status());
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi conectada.");
+  Serial.println("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+ 
+  server.begin();
+}
+ 
+void loop() {
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("Novo Cliente.");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.println("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>");
+            client.print("Click <a class='btn btn-success' style='margin: 5px' href=\"/H\">here</a> to turn the LED on pin 2 on.<br>");
+            client.print("Click <a class='btn btn-danger' style='margin: 5px' href=\"/L\">here</a> to turn the LED on pin 2 off.<br>");
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(LED, HIGH);
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(LED, LOW);
+        }
+      }
+    }
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
